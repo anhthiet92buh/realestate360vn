c0112d51a603224d1b2eaebd1b822f8792052a2f
Vo Ta Dung <47474729+votadung2000@users.noreply.github.com>
2023-04-14 11:51:55 +0700

    remove config babel-plugin-module-resolver

--- src/screen/AppContainer.js
+++ src/screen/AppContainer.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import routes from '@routes';
+import HomeScreen from './home/HomeScreen'
 
 const Stack = createNativeStackNavigator();
 
 const AppContainer = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={routes.HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: false,
+          animation: 'slide_from_right',
+        }}>
+        <Stack.Screen name={"HomeScreen"} component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
